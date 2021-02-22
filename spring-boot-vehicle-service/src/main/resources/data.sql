7f0712c38dbb61278aa88a97bb529628f3c99111
Jayaram Mani <jayaram.h4u@gmail.com>
2021-02-22 01:13:01 +0000

    add additional initial data into `data.sql`

--- spring-boot-vehicle-service/src/main/resources/data.sql
+++ spring-boot-vehicle-service/src/main/resources/data.sql
@@ -41,22 +41,11 @@ INSERT INTO make (name) VALUES
 
 INSERT INTO model (make_id, name) VALUES
     (1, 'A1'),
-    (1, 'A2'),
     (1, 'A3'),
-    (1, 'A4'),
     (1, 'Q3'),
-    (1, 'Q5'),
     (2, '1 series'),
-    (2, '2 series'),
     (2, '3 series'),
-    (2, '4 series'),
-    (2, 'X3'),
-    (2, 'X5'),
-    (3, 'Fiesta'),
-    (3, 'Focus'),
-    (3, 'Galaxy'),
-    (3, 'Puma'),
-    (3, 'Kuga');
+    (2, 'X5');
 
 INSERT INTO model_edition (make_id, model_id, name, description, base_price) VALUES
     (1, 1, 'Sportback Attraction', 
@@ -80,8 +69,64 @@ INSERT INTO model_edition (make_id, model_id, name, description, base_price) VAL
             * 2 Zone Climate Control
             * SE Bumpers
             * Split-folding rear seat',
-        35894.81);
+        35894.81),
+    (1, 2, 'Sportback S-line', 
+        'Sportback S-line model - 
+            * Leather seats
+            * Front sports seats
+            * 18” alloy wheels
+            * Sports suspension
+            * Flat-bottomed steering wheel
+            * LED Front lights with LED dynamic rear lights
+            * Audi drive select
+            * S line exterior styling
+             - Roof spoiler
+             - Sports front and rear bumpers
+            * S line interior elements
+             - Stainless steel pedals
+             - S line scuff plates in doors
+             - S line steering wheel
+             - Black headliner
+             - Split folding rear seat back rest with centre armrest and cupholders
+             - Armrest in leather',
+        38280.14),
+    (1, 3, 'Sportback SE', 
+        'Saloon SE model - 
+            * 18” 5-arm alloy wheels
+            * LED headlamps (Low)
+            * Audi PreSense basic & PreSense front
+            * Audi smartphone interface
+            * Dual Zone climate control
+            * Audi eCall
+            * Hill hold and descent control
+            * Virtual cockpit -10.25” screen
+            * Electric tailgate',
+        42011.69),
+    (2, 4, 'SE', 
+        'Hatchback SE model - 
+            The SE model marks the entry point to the range, offering a high level of standard equipment and trademark BMW driving dynamics.',
+        32891.56),
+    (2, 4, 'M Sport', 
+        'Hatchback M sport - 
+            The M Sport model offers a sharpened appearance with M aerodynamic bodystyling, High-gloss Shadowline exterior trim and upgraded M light alloy wheels. Inside, the sporty feel is further enhanced by the leather upholstery and M-specific steering wheel.',
+        38325.42),
+    (2, 4, 'M Models', 
+        'Hatchback M models - 
+            BMW M Models are born on the racetrack. Inspired by BMWs rich motorsport history, M Models offer an outstanding combination of agility, dynamics and power. Equipped with powerful engines and M design enhancements, M Models are a statement of performance.',
+        53977.79),
+    (2, 5, 'SE Touring', 
+        'SE Touring - 
+            The SE model marks the entry point to the range, offering a high level of standard equipment and trademark BMW driving dynamics.',
+        46425.01),
+    (2, 6, 'Sport Touring', 
+        'Sport Touring - 
+            The Sport model offers a more athletic look with High-gloss Black exterior design elements as well as upgraded light alloy wheels. Interior highlights include Sport seats for the driver and front passenger and an exclusive Sport interior trim.',
+        48445.01);
 
 INSERT INTO model_option (model_edition_id, name, price) VALUES
     (1, '16 alloy wheels', 1478.88),
-    (1, 'Cruise control', 3250.78);
+    (1, 'Cruise control', 3250.78),
+    (1, 'Rear parking sensor', 567.78),
+    (2, '18 alloy wheels', 2478.88),
+    (2, 'Heated seats', 2890.78),
+    (2, 'Rear parking camera', 1567.78);
